6b5ce0f6c098fae7be9cfd6a5693f136f61ab781
nateVanDeVeire <bluefury6@gmail.com>
2023-11-06 19:42:28 -0800

    added controller and ControlMap

--- src/main/java/frc/robot/subsystems/controls/controller.java
+++ src/main/java/frc/robot/subsystems/controls/controller.java
@@ -1,3 +1,7 @@
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
 public class controller {
-    
+    public static void initTeleop() {
+        new JoystickButton(ControlMap, 0)
+    }
 }
