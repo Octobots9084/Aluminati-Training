9f6a53012a4d9adc5878d2dcc0d1c7359b8a677d
OlorinOut <sebastian@fedaykin.net>
2023-12-04 19:32:57 -0800

    pushed when battery was dying SO DONT TRUST

--- src/main/java/frc/robot/subsystems/Arms/RevKinem.java
+++ src/main/java/frc/robot/subsystems/Arms/RevKinem.java
@@ -3,11 +3,20 @@ package frc.robot.subsystems.Arms;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 public class RevKinem extends InstantCommand {
-    private static double absWristAngle;
-    private static double targetX;
-    private static double targetY;
+    private double absWristAngle;
+    private double targetX;
+    private double targetY;
 
     public RevKinem (double wristAngle, double targetX, double targetY) {
-        wristAngle = absWristAngle  
+        wristAngle = absWristAngle;
+        this.targetX = targetX;
+        this.targetY = targetY; 
+    }
+
+    public static void main(String[] args)
+    {
+        CalcRevKinem.setVariables(0.82, 3, 1);
+        System.out.println(CalcRevKinem.calcPivotAngle());
+        System.out.println(CalcRevKinem.calcExtensionDistance());
     }
 }
